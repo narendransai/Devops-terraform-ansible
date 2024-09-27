62b9201dfc598c88faaf35f94b0c211b955536a2
narendransai <narendransai@gmail.com>
2024-09-27 14:48:47 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
+  name        = "narenssh"
   description = "Allow SSH inbound traffic"
   vpc_id      = "vpc-00dae5f3df962676d"
 
